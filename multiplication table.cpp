9f3d61c52dc5a7ff0494d30615e6350432489a98
Your Name <nika.neirami@gmail.com>
2022-02-28 13:55:22 +0330

    add

---
+++ multiplication table.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main ()
+{ 
+int i,j;
+for( i=1;i<11;i++)
+{
+	cout<<endl;
+	
+	for(j=1;j<11;j++)
+	
+		cout<<(i*j)<<" ";
+		cout<<endl;	
+}
+	
+	return 0;
+}
+	
+	
+	
+	
+
+	
+
